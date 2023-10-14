1e70dca5bb70cb6632f4daa3231c8e4d896864e1
GhostMobin <s1810677110@ru.ac.bd>
2023-10-14 13:37:19 +0600

    update feature

--- index.js
+++ index.js
@@ -44,6 +44,31 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const user = await usersCollection.findOne(query);
+            res.send(user);
+        })
+
+
+
+        app.put("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            console.log("Updating", id, user);
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: user.name,
+                    email: user.email,
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
         app.delete("/users/:id", async (req, res) => {
             const id = req.params.id;
             console.log("deleting", id);
