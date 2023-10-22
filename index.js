a0ee87dc3456befb2a1678c70dad0892b462a4ff
GhostMobin <s1810677110@ru.ac.bd>
2023-10-22 22:33:56 +0600

    env added

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
-
-const uri = "mongodb+srv://mobin:m0bin@cluster0.i01a3kb.mongodb.net/?retryWrites=true&w=majority";
-// const uri = "mongodb+srv://mobin:m0b1n@cluster1.pvwuryt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.i01a3kb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
